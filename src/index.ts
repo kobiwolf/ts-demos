3bd5ffe049ec6471814a639b24e7288ed4a679c8
kobi wolf <kobiwolf95@gmail.com>
2021-06-03 12:53:53 +0300

    a

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-const b: number = 5;
-console.log(b);
-let c: 12 = 12;
+//! using [] as type
+// let a: { c: string; [b: number]: number };
+// a = { c: 'a', 5: 6 };
+// console.log(a)
 
+//! using read
+// let a: { readonly b: string } = { b: 'a' };
+// let b:readonly-cant b used
+// a.b='c'
+// console.log(a);
+
+// !declare obj
+let a: object;
+a = { c: 'n' };
+console.log(a);
+let b: Object = { c: 'v' };
+console.log(b);
+let c: { a: string };
+c = { a: 'a' };
 console.log(c);
